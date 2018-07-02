39f8c341ffce16326f990df70bfb2903be7ae666
Norbert Szulc <norbert@not7cd.net>
2018-07-02 23:50:06 +0200

    Things that work

--- finansonator.py
+++ finansonator.py
@@ -1,11 +1,17 @@
-import click
-import dokuwiki
-import crayons
-import os
-
 from datetime import datetime
+from getpass import getpass
+
+import click
+import crayons
+import dokuwiki
+import tablib
 from jinja2 import Template
 from tabulate import tabulate
+
+from helpers import _normalize_mbank_headers, report_balance, report_date, \
+    last_balance
+
+cli = click.Group()
 
 report_page = Template(
     """
@@ -16,46 +22,72 @@ report_page = Template(
 """
 )
 
-
 WIKI_URL = "https://wiki.hs3.pl"
 WIKI_PAGE = ":finanse:bilanse"
 
 
-def aggregate_data():
-    return [("przychody", 1000), ("wydatki", -700), ("bilans", 300)]
-
-
-# TODO: https://docs.python.org/3/library/getpass.html#getpass.getpass
-
-@click.command()
-@click.option("--user", prompt="Username", default=lambda: os.environ.get("USER", ""))
-@click.option(
-    "--password",
-    prompt="password",
-    hide_input=True,
-    default=lambda: os.environ.get("PASSWORD", ""),
-)
-def connect(user, password):
-    """Simple program that greets NAME for a total of COUNT times."""
+def create_wiki_session():
+    """returns dokuwiki session, aiming for safer experience"""
+    print(crayons.yellow("Enter credentials for {}".format(WIKI_URL)))
+    user = input("User: ")
+    password = getpass("Password: ")
 
     try:
         wiki = dokuwiki.DokuWiki(WIKI_URL, user, password)
     except dokuwiki.DokuWikiError as exc:
         print(crayons.red(exc))
+        exit()
     else:
         print(crayons.green("Connected to {}".format(wiki.title)))
+        return wiki
 
-    print(crayons.yellow("Rendering template"))
 
+
+@click.option("--dry-run", "dry_run", is_flag=True)
+def post_report(wiki, dry_run, report):
+    """Simple program that greets NAME for a total of COUNT times."""
     content = report_page.render(
-        title=datetime.today(), table=tabulate(aggregate_data(), tablefmt="youtrack")
+        title="{}.{}".format(report["year"], report["month"]), table=tabulate(list(report.items())[:-2], tablefmt="youtrack")
     )
 
     print(content)
 
-    print(crayons.yellow("Appending to wiki page {}".format(WIKI_PAGE)))
-    wiki.pages.append(WIKI_PAGE, content=content)
+    if not dry_run:
+        print(crayons.green("Appending to wiki page {} DokuWiki".format(WIKI_PAGE)))
+        wiki.pages.append(WIKI_PAGE, content=content)
+    else:
+        print(crayons.yellow("Would append to page {} at DokuWiki".format(WIKI_PAGE)))
+
+
+@click.option("-i", "--input", "src", type=click.File("r"))
+@click.option("-v", "--verbose", is_flag=True, help="Enables verbose mode")
+@click.option(
+    "-n", "--dry-run", is_flag=True, help="Will print out changes without applying them"
+)
+def aggregate(src, verbose, dry_run):
+    if src is not None:
+        data = tablib.import_set(src.read(), "csv", delimiter=";")
+        data.headers = _normalize_mbank_headers(data.headers)
+
+        aggregated = {**report_balance(data), **report_date(data), "balance": last_balance(data)}
+
+        if verbose:
+            click.echo(aggregated)
+        return aggregated
+
+
+@click.command()
+@click.option("-i", "--input", "src", type=click.File("r"))
+@click.option("-v", "--verbose", is_flag=True, help="Enables verbose mode")
+@click.option(
+    "-n", "--dry-run", is_flag=True, help="Will print out changes without applying them"
+)
+def main(src, dry_run, verbose):
+    report = aggregate(src, verbose, dry_run)
+
+    wiki = create_wiki_session()
+    post_report(wiki=wiki, dry_run=dry_run, report=report)
 
 
 if __name__ == "__main__":
-    connect()
+    main()
---
+++ helpers.py
@@ -0,0 +1,36 @@
+from glom import glom
+from datetime import datetime
+
+
+def _normalize_mbank_headers(headers):
+    return [h.replace("#", "") for h in headers]
+
+
+def _normalize_float(string):
+    return float(string.strip().replace(",", ".").replace(" ", ""))
+
+
+def report_balance(data):
+    """Simple function returning income and spendings"""
+    transactions = glom(data.dict, ["Kwota"])
+    transactions = [_normalize_float(t) for t in transactions]
+
+    expenses = sum(t for t in transactions if t < 0)
+    income = sum(t for t in transactions if t > 0)
+    balance = sum(transactions)
+
+    return {"income": income, "expenses": expenses, "balance": balance}
+
+
+def report_date(data):
+    dts = glom(data.dict, [lambda d: datetime.strptime(d["Data operacji"], '%Y-%m-%d')])
+    assert all((dt.year, dt.month) == (dts[0].year, dts[0].month) for dt in dts)
+    return {"year": dts[0].year, "month": dts[0].month}
+
+
+def last_balance(data):
+    return _normalize_float(data.dict[-1]["Saldo po operacji"])
+
+
+# @click.group(chain=True, invoke_without_command=True)
+
